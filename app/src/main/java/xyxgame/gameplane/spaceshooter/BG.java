12325ac4a03dc4289188119a4983a0b528679f85
Administrator <316375076@qq.com>
2020-12-01 23:40:07 +0800

    2020.12

--- app/src/main/java/xyxgame/gameplane/spaceshooter/BG.java
+++ app/src/main/java/xyxgame/gameplane/spaceshooter/BG.java
@@ -3,25 +3,45 @@ package xyxgame.gameplane.spaceshooter;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 
 import xyxgame.gameplane.R;
 
 public class BG {
-    Bitmap mBitmap;//
+    private Bitmap bitmap;
+    private int y1,y2,screenSizeX,screenSizeY;
 
 
-    public BG(Context context, int screenSizeX, int screenSizeY) {
-
-
-        mBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.bg1);
-
-
-        mBitmap = Bitmap.createScaledBitmap(mBitmap, screenSizeX, screenSizeY, false);
+    public BG(Context context,int screenSizeX, int screenSizeY) {
+        this.bitmap  = BitmapFactory.decodeResource(context.getResources(), R.drawable.bg2);
+        y2 = y1 -bitmap.getHeight();
+        this. screenSizeX=screenSizeX;
+        this. screenSizeY=screenSizeY;
     }
 
 
 
-    public Bitmap getmBitmap() {
-        return mBitmap;
+    public void draw(Canvas canvas, Paint paint){
+        canvas.drawBitmap(bitmap,0,y1,paint);
+        canvas.drawBitmap(bitmap,0,y2,paint);
+        update();
     }
+
+    public void update(){
+        y1 +=1;
+        y2 +=1;
+        if(y1>screenSizeY){
+            y1 = y2 -bitmap.getHeight();
+        }
+        if (y2>screenSizeY){
+            y2 = y1 - bitmap.getHeight();
+        }
+    }
+
+
+
+
+
+
 }
