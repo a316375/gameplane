f32d7840caaef928c8f457ff06207375740bac53
Administrator <316375076@qq.com>
2020-12-12 10:10:08 +0800

    2020.13-SchoolGame-01

--- app/src/main/java/xyxgame/gameplane/school/Students/StudentB.java
+++ app/src/main/java/xyxgame/gameplane/school/Students/StudentB.java
@@ -18,74 +18,77 @@ import xyxgame.gameplane.Base.BaseStudent;
 public class StudentB extends BaseStudent {
 
 
-    private float[] afP;
+    private float[]  afP = new float[]{0f, 0f};;
     private Path path;
+    private Paint paint;
+    private PathMeasure pm;
+    private float fSegmentLen;
 
     public StudentB(BaseBag baseBag) {
         super(  baseBag);
         mx_BaseStudent =point.x/2;
         my_BaseStudent =0;
 
+        paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);  // 填充模式 - 描边
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.RED);
+
+        //小球跟随路径移动
+        pm = new PathMeasure(path, false);
+
+
 
     }
 
 
     int starty=50,stopy=50;
-    int iCurStep = 0;// current animation step
+    int startx=0;
 
-    int j=0,m=0;
+
+
 
     @Override
     public void draw(Canvas canvas) {
 
 
-if (is_over==false) {
-    Paint paint = new Paint();
-    paint.setStyle(Paint.Style.STROKE);  // 填充模式 - 描边
-    paint.setStrokeWidth(5);
-    paint.setColor(Color.RED);
-    path = new Path();
+    if (is_over==true) return;
+        path = new Path();
+        path.moveTo(startx, starty);
+        path.lineTo(point.x, stopy);
+     //   canvas.drawPath(path, paint);
 
-    path.moveTo(0, starty);
-    path.lineTo(point.x*2, stopy);
+        pm = new PathMeasure(path, false);
 
+        //we'll get 200 points from path to animate the circle
+        fSegmentLen = pm.getLength() / 2000;
 
-    canvas.drawPath(path, paint);
-
-    //小球跟随路径移动
-    PathMeasure pm = new PathMeasure(path, false);
-    float fSegmentLen = pm.getLength() / 1000;//we'll get 200 points from path to animate the circle
-    afP = new float[]{0f, 0f};
-
-
-    if (iCurStep <= 1000) {
-        boolean posTan = pm.getPosTan(fSegmentLen * iCurStep, afP, null);
-        // canvas.drawCircle(afP[0], afP[1],20,paint);
         canvas.drawBitmap(bitmap, afP[0], afP[1], paint);
-        if (j < afP[0]) j++;
-        if (m < afP[1]) j++;
-
-        iCurStep++;
-        // invalidate();
-    } else {
-        Log.e("draw: ", j + "---" + m);
-
-        iCurStep = 0;
-    }
-    ;
-}
-       // super.draw(canvas);
+      // super.draw(canvas);
     }
 
+    int iCurStep = 0;// current animation step
     @Override
     public void drawXP() {
 
-         starty+=2;stopy+=2;
+        starty+=2;stopy+=2;
+
+
+
+//让他移动到屏幕1/3的地方停下来
+        if (iCurStep <= point.x/3) {
+            pm.getPosTan(fSegmentLen * iCurStep, afP, null);
+            iCurStep++;
+            // invalidate();
+        } else {
+            iCurStep = point.x/3;
+        }
+
 
          mx_BaseStudent=afP[0];
          my_BaseStudent=afP[1];
 
-         if (mx_BaseStudent>point.x)is_over=true;
+        // if (mx_BaseStudent>point.x)is_over=true;
 
 
 
