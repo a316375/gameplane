e3fcd48b043d0066c0aede9674edb29f1795ccdc
Administrator <316375076@qq.com>
2020-12-14 22:08:08 +0800

    2020.13-SchoolGame-GIF02

--- app/src/main/java/xyxgame/gameplane/GIf/BaseGIF.java
+++ app/src/main/java/xyxgame/gameplane/GIf/BaseGIF.java
@@ -54,6 +54,15 @@ public class BaseGIF {
    int iCurStep =0; int stopx=new Random().nextInt(1000);int stopy=new Random().nextInt(1000);
     public void draw(Canvas canvas){
         if (bitmaps.size()==0)return;
+
+
+        pathb(canvas);
+
+
+
+
+
+
         j++;
          i=i%bitmaps.size();
 
@@ -65,10 +74,22 @@ public class BaseGIF {
 
 
 
-        //以下是路径，待封装
 
+
+
+
+
+
+
+
+    }
+
+    private void patha(Canvas canvas) {
+
+        //以下是路径，待封装，负责计算坐标
         Point a=new Point(xX,xY);
         Point b=new Point(stopx,stopy);
+
         xY+=2;
         stopy+=2;
 
@@ -88,19 +109,49 @@ public class BaseGIF {
 
         if (iCurStep <=300) {
             pm.getPosTan(fSegmentLen * iCurStep, afP, null);
-           //canvas.drawBitmap((Bitmap) bitmaps.get(i),xX,xY,null);
-          xX= (int) afP[0];xY= (int) afP[1];
-          if (j %time==0)   iCurStep++;
+            xX= (int) afP[0];xY= (int) afP[1];
+            if (j %time==0)   iCurStep++;
             // invalidate();
         } else {
             iCurStep = 300;
         };
-
-
-
-
-
-
-
     }
+
+    int cenx=0,ceny=0;
+
+    private void pathb(Canvas canvas) {
+
+        //以下是路径，待封装，负责计算坐标
+        Point a=new Point(xX,xY);
+        Point b=new Point(stopx,stopy);
+
+
+
+
+
+        Path sPath = new Path();
+        sPath.moveTo(a.x, a.y);
+        sPath.quadTo(b.x, b.y,500,200);
+        PathMeasure pm = new PathMeasure(sPath, false);
+        Paint pathPaint = new Paint();
+        pathPaint.setStyle(Paint.Style.STROKE);  // 填充模式 - 描边
+        pathPaint.setStrokeWidth(1);
+        pathPaint.setColor(Color.BLUE);
+
+        canvas.drawPath(sPath,pathPaint);
+
+        float fSegmentLen = pm.getLength() / 2000;//we'll get 20 points from path to animate the circle
+        float afP[] = {0f, 0f};
+
+        if (iCurStep <=300) {
+            pm.getPosTan(fSegmentLen * iCurStep, afP, null);
+            xX= (int) afP[0];xY= (int) afP[1];
+            if (j %time==0)   iCurStep++;
+            // invalidate();
+        } else {
+            iCurStep = 300;
+        };
+    }
+
+
 }
--- app/src/main/java/xyxgame/gameplane/GIf/BasePath.java
+++ app/src/main/java/xyxgame/gameplane/GIf/BasePath.java
@@ -1,9 +1,56 @@
 package xyxgame.gameplane.GIf;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.graphics.Point;
+
 public class BasePath {
 
     public static float[] linePath(){
         float[] floats={0,0};
         return floats;
     }
+
+
+
+
+//
+//    private void patha(Canvas canvas) {
+//
+//        //以下是路径，待封装，负责计算坐标
+//
+//        Point a=new Point(xX,xY);
+//        Point b=new Point(stopx,stopy);
+//        xY+=2;
+//        stopy+=2;
+//
+//        Path sPath = new Path();
+//        sPath.moveTo(a.x, a.y);
+//        sPath.lineTo(b.x, b.y);
+//        PathMeasure pm = new PathMeasure(sPath, false);
+//        Paint pathPaint = new Paint();
+//        pathPaint.setStyle(Paint.Style.STROKE);  // 填充模式 - 描边
+//        pathPaint.setStrokeWidth(1);
+//        pathPaint.setColor(Color.BLUE);
+//
+//        canvas.drawPath(sPath,pathPaint);
+//
+//        float fSegmentLen = pm.getLength() / 2000;//we'll get 20 points from path to animate the circle
+//        float afP[] = {0f, 0f};
+//
+//        if (iCurStep <=300) {
+//            pm.getPosTan(fSegmentLen * iCurStep, afP, null);
+//            xX= (int) afP[0];xY= (int) afP[1];
+//            if (j %time==0)   iCurStep++;
+//            // invalidate();
+//        } else {
+//            iCurStep = 300;
+//        };
+//    }
+
+
+
 }
